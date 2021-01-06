0ac14834e31046be206b09abf5a130c23e76afac
Meng Xiao <mxiaopsu@gmail.com>
2021-01-06 11:35:04 -0500

    get api working

--- psql_ad_restapi.py
+++ psql_ad_restapi.py
@@ -1,14 +1,15 @@
 from flask import Flask
-from flask_restful import Api, Resource, reqparse, abort
+from flask_restful import Api, Resource, reqparse, abort, fields, marshal_with
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 api = Api(app)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://meng:IAOeng19910804@localhost/simglucose'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://meng:iaoeng@localhost:5432/simglucose'
 db = SQLAlchemy(app)
 
 class Results(db.Model):
     result_id = db.Column(db.Integer, primary_key=True)
+    time = db.Column(db.DateTime, nullable=False)
     bg = db.Column(db.Float)
     cgm = db.Column(db.Float)
     cho = db.Column(db.Float)
@@ -19,12 +20,21 @@ class Results(db.Model):
     patient_id = db.Column(db.String, nullable=False)
 
     def __repr__(self):
-        return f"Results(BG = {bg}, CGM = {cgm}, CHO = {cho}, INSULIN = {insulin}, LBGI = {lbgi}, HBGI = {hbgi}, RISK = {risk}, Patient_ID = {patient_id})"
+        return f"Results(Time = {time}, BG = {bg}, CGM = {cgm}, CHO = {cho}, INSULIN = {insulin}, LBGI = {lbgi}, HBGI = {hbgi}, RISK = {risk}, Patient_ID = {patient_id})"
+    
+# resource_fields = {
+# 	'patient_id': fields.String,
+# 	'bg': fields.Float,
+# 	'cgm': fields.Float,
+# 	#'likes': fields.Integer
+# }
 
 class Simglucose(Resource):
+    # @marshal_with(resource_fields)
     def get(self, patient_id):
+        print(patient_id)
         result = Results.query.filter_by(patient_id=patient_id).all()
-        return result
+        return result.json()
 
 api.add_resource(Simglucose, "/Simglucose/<string:patient_id>")
 
