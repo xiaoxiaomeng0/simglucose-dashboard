0076e47223c2a0c0242ce8e18c81ec867231cdde
xiaoxiaomeng0 <mxiaopsu@gmail.com>
2021-02-01 11:02:16 -0500

    Update dropdown button with all the patient name

--- simglucose_web/src/components/patientIDDropdown.js
+++ simglucose_web/src/components/patientIDDropdown.js
@@ -4,17 +4,23 @@ import Dropdown from 'react-bootstrap/Dropdown';
 class PatientIDDropdown extends Component {
     
     render()  {
-        const {handleChange, selectedValue} = this.props 
+        const {fetchPatientIDItems, handleChange, selectedValue} = this.props
+        
         return ( 
             <Dropdown>
             <Dropdown.Toggle variant="success" id="dropdown-basic">
                 {selectedValue}
             </Dropdown.Toggle>
             
-            <Dropdown.Menu>
-                <Dropdown.Item eventKey="adult#001" onSelect={handleChange}>adult#001</Dropdown.Item>
-                <Dropdown.Item eventKey="adult#002" onSelect={handleChange}>adult#002</Dropdown.Item>
-                <Dropdown.Item eventKey="adult#003" onSelect={handleChange}>adult#003</Dropdown.Item>
+            <Dropdown.Menu> {
+                fetchPatientIDItems.map((patientID) => 
+                <Dropdown.Item
+                key={patientID}
+                eventKey={patientID} 
+                onSelect={handleChange}>
+                {patientID}
+                </Dropdown.Item>)
+            }
             </Dropdown.Menu>
             </Dropdown>
          );
